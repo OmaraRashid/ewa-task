8c30db64dcd315f7a8fd065dc94afb9b47d19656
OmaraRashid <omararashed1@gmail.com>
2024-01-30 20:47:59 +0200

    handle-auto-complete-functiolaity-bind

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+   {
+    path: 'signup',
+    loadChildren: () => import('./mobile-query/mobile-query.module').then(m => m.MobileQueryModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
